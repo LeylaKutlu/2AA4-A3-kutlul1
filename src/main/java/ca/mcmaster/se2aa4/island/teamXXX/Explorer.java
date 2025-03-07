9b8d23a080a219badeb66a4527b4414a0d06a452
Fatima Alsalahi <alsalahf@mcmaster.ca>
2025-03-07 13:54:26 -0500

    close #6 Modified Explorer

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Explorer.java
@@ -11,7 +11,7 @@ import org.json.JSONTokener;
 public class Explorer implements IExplorerRaid {
 
     private final Logger logger = LogManager.getLogger();
-    private int batteryLevel;
+    private Drone drone;
     private String direction;
     private Response previousResponse = null;
     private JSONObject previousDecision = null;
@@ -22,7 +22,8 @@ public class Explorer implements IExplorerRaid {
         JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
         logger.info("** Initialization info:\n {}",info.toString(2));
         this.direction = info.getString("heading");
-        this.batteryLevel = info.getInt("budget");
+        int batteryLevel = info.getInt("budget");
+        this.drone = new Drone(batteryLevel, direction);
         logger.info("The drone is facing {}", direction);
         logger.info("Battery level is {}", batteryLevel);
     }
@@ -30,47 +31,47 @@ public class Explorer implements IExplorerRaid {
     @Override
     public String takeDecision() {
 
-        JSONObject decision = new JSONObject();
+        Decision decision = new JSONObject();
         JSONObject parameters = new JSONObject();
         
-        if (batteryLevel < 2) {
-            decision.put("action", "stop");
+        if (drone.getBatteryLevel() < 2) {
+            decision.setAction("stop");
             return decision.toString();
         } 
         
         if (previousDecision == null){
-            decision.put("action", "fly");
-        } else if (direction.equals("S") && previousDecision.getString("action").equals("heading")) {
-            decision.put("action", "echo");
+            decision.setAction("fly");
+        } else if (direction.equals("S") && previousDecision.getAction.equals("heading")) {
+            decision.setAction("echo");
             parameters.put("direction", "E");
-            decision.put("parameters", parameters);
-        } else if (!previousDecision.getString("action").equals("echo")) {
-            decision.put("action", "echo"); 
+            decision.setParameters(parameters);
+        } else if (!previousDecision.getAction.equals("echo")) {
+            decision.setAction("echo"); 
             if (direction.equals("E")) {
                 parameters.put("direction", "E");
             } else if (direction.equals("W")){
                 parameters.put("direction", "W");
             }
-            decision.put("parameters", parameters);
+            decision.setParameters(parameters);
         } else if(direction.equals("S") && previousResponse.getRange() == 0){
-            decision.put("action", "heading");
+            decision.setAction("heading");
             parameters.put("direction", "W");
             direction = "W";
-            decision.put("parameters", parameters);
+            decision.setParameters(parameters);
         } else if (direction.equals("S") && previousResponse.getRange() > 0){
-            decision.put("action", "heading");
+            decision.setAction("heading");
             parameters.put("direction", "E");
             direction = "E";
-            decision.put("parameters", parameters); 
+            decision.setParameters(parameters);
         } else if (previousResponse.getRange() == 1 && (direction.equals("E") || direction.equals("W"))) {
-            decision.put("action", "heading"); 
+            decision.setAction("heading"); 
             parameters.put("direction", "S"); 
             direction = "S";
-            decision.put("parameters", parameters);
+            decision.setParameters(parameters);
         } else if (previousResponse.groundFound()) {
-            decision.put("action", "stop");
+            decision.setAction("stop");
         } else if (!previousResponse.groundFound()) {
-            decision.put("action", "fly");
+            decision.setAction("fly");
         }
 
         logger.info("** Decision: {}",decision.toString(4));
@@ -86,13 +87,13 @@ public class Explorer implements IExplorerRaid {
         logger.info("** Response received:\n"+response.getResponse());
         Integer cost = response.getCost();
         logger.info("The cost of the action was {}", cost);
-        batteryLevel -= cost;
+        drone.decreaseBattery(cost);
         String status = response.getStatus();
         logger.info("The status of the drone is {}", status);
         JSONObject extraInfo = response.getExtras();
         logger.info("Additional information received: {}", extraInfo);
         previousResponse = response;
-        logger.info("Battery level is now {}", batteryLevel);
+        logger.info("Battery level is now {}", drone.getBatteryLevel());
     }
 
     @Override
