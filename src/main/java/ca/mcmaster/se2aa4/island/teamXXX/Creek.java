b2bccb0254240f0ab1c8625227dc7f81368f7626
Fatima Alsalahi <alsalahf@mcmaster.ca>
2025-03-16 14:23:32 -0400

    close #12 Created Creek Class

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Creek.java
@@ -0,0 +1,28 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+import java.util.ArrayList;
+import java.until.List;
+
+public class Creek {
+    private String creekId;
+    private int x;
+    private int y;
+
+    public Creek(String creekId, int x, int y) {
+        this.creekId = creekId;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getCreekId() {
+        return creekId;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
