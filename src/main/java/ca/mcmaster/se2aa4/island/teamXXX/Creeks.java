5c332acf72773e827c1efaecb15ec3fdc7ce0a48
Fatima Alsalahi <alsalahf@mcmaster.ca>
2025-03-16 14:24:10 -0400

    close #13 Created Creeks Class

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Creeks.java
@@ -0,0 +1,21 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+import java.util.ArrayList;
+import java.until.List;
+
+public class Creeks {
+
+    private List<Creek> creeks;
+
+    public Creeks() {
+        this.creeks = new ArrayList<>();
+    }
+
+    public void addCreek(Creek creek) {
+            creeks.add(creek);
+    }
+
+    public List<Creek> getCreeks() {
+        return new ArrayList<>(creeks);
+    }
+}
