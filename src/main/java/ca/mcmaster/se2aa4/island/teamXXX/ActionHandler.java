91d16766e7acc0074706a8e2abae3e4d4f2e8652
Rachel Nyonje <rachelnyonje@gmail.com>
2025-03-23 12:33:28 -0400

    Removed static methods.

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/ActionHandler.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/ActionHandler.java
@@ -3,29 +3,29 @@ package ca.mcmaster.se2aa4.island.teamXXX;
 import org.json.JSONObject;
 
 public class ActionHandler {
-    public static void fly(Decision decision) {
+    public void fly(Decision decision) {
         decision.setAction(Action.FLY);
     }
 
-    public static void echo(Decision decision, Direction direction) {
+    public void echo(Decision decision, Direction direction) {
         JSONObject parameters = new JSONObject();
         parameters.put("direction", direction.toString());
         decision.setAction(Action.ECHO);
         decision.setParameters(parameters);
     }
 
-    public static void heading(Decision decision, Direction direction) {
+    public void heading(Decision decision, Direction direction) {
         JSONObject parameters = new JSONObject();
         parameters.put("direction", direction.toString());
         decision.setAction(Action.HEADING);
         decision.setParameters(parameters);
     }
 
-    public static void stop(Decision decision) {
+    public void stop(Decision decision) {
         decision.setAction(Action.STOP);
     }
 
-    public static void scan(Decision decision) {
+    public void scan(Decision decision) {
         decision.setAction(Action.SCAN);
     }
 }
